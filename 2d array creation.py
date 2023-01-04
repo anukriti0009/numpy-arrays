1c79d9b8790a7f363f3b26639075f1e8c224d22e
ANUKRITI <88333460+anukriti0009@users.noreply.github.com>
2023-01-04 23:01:19 +0530

    Add files via upload

---
+++ 2d array creation.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+import numpy as np 
+x = np.eye(3)
+print(x)
+
+x = np.eye(3,5)
+print(x)
+
+np.diag([1,2,3])
+
+np.diag([1,2,3],1)
+
+a = np.array([[1,2],[3,4]])
+np.diag(a)
+
+
